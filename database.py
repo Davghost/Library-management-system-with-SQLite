b970e39632845d571decf64988f2de12e951f70b
davi <acrookedfork@gmail.com>
2026-01-03 02:13:07 -0300

    feat: system initial commit

---
+++ database.py
@@ -0,0 +1,146 @@
+from init_db import Reader, Book, Loan, engine, BaseService
+from sqlalchemy.orm import sessionmaker
+from datetime import date
+
+Session = sessionmaker(bind=engine)
+
+class ReaderService(BaseService):
+   @staticmethod
+   def register(name:str):
+      session = Session()
+      try:
+         reader = Reader(name=name)
+         return ReaderService.create(session, reader)
+      finally:
+         #print("reader added successfully")
+         session.close()
+
+   @staticmethod
+   def update(reader_id:int, reader_name:str):
+      session = Session()
+      try:
+         reader = session.get(Reader, reader_id)
+         if not reader:
+            raise ValueError("Reader not found")
+         
+         reader.readers_name = reader_name
+         BaseService.update(session)
+         return reader
+      finally:
+         session.close()
+
+   @staticmethod
+   def delete(reader_id:int):
+      session = Session()
+      try:
+         reader = session.get(Reader, reader_id)
+         if reader is None:
+            raise ValueError("Reader not found")
+         
+         BaseService.delete(session, reader)
+      finally:
+         session.close()
+
+class BookService(BaseService):
+   @staticmethod
+   def register(title:str, author:str):
+      session = Session()
+      try:
+         book = Book(title=title, author=author)
+         return BookService.create(session, book)
+      finally:
+         #print("book added successfully")
+         session.close()
+   
+   @staticmethod
+   def update(book_id:int, book_title:str | None=None, book_author:str | None=None):
+      session = Session()
+      try:
+         book = session.get(Book, book_id)
+         if not book:
+            raise ValueError("Book not found")
+         
+         if book_title is not None:
+            book.title = book_title
+
+         if book_author is not None:
+            book.author = book_author
+
+         BaseService.update(session)
+         return book
+      finally:
+         session.close()
+      
+class StructuredDataReturn():
+   @staticmethod
+   def return_all_readers():
+      with Session(engine) as session:
+         readers = session.query(Reader).all()
+
+         return [
+            {
+               "id": reader.id,
+               "name": reader.readers_name,
+            }
+            for reader in readers
+         ]
+   
+   @staticmethod
+   def return_all_books():
+      with Session(engine) as session:
+         books = session.query(Book).all()
+         return [
+             {
+                 "id": book.id,
+                 "title": book.title,
+                 "author": book.author
+             }
+             for book in books
+         ]
+
+class LibraryService():
+   @staticmethod
+   def loan(book_id:int, reader_id:int):
+      session = Session()
+      try:
+         reader = session.get(Reader, reader_id)
+         
+         if reader is None:
+            raise ValueError("Reader not found")
+         
+         free = BaseService.check_free_loan(session, book_id)
+         if not free:
+            raise ValueError("Book is already loaned")
+
+         loan = Loan(
+            book_id = book_id,
+            reader_id = reader_id,
+            loan_date = date.today(),
+            return_date = None
+         )
+
+         BaseService.create(session, loan)
+         return loan
+               
+      finally:
+         session.close()
+
+   @staticmethod
+   def book_return(book_id:int):
+      session = Session()
+      try:
+         loan = session.query(Loan).filter(
+            Loan.book_id == book_id,
+            Loan.return_date == None
+         ).first()
+
+         if loan is None:
+            raise ValueError("No active loan found for this book")
+         
+         Loan.return_date = date.today()
+         BaseService.update(session)
+
+         return loan
+      
+      finally:
+         session.close()
---
+++ init_db.py
@@ -0,0 +1,72 @@
+from sqlalchemy import create_engine, Column, Integer, UnicodeText, Unicode, ForeignKey, Date
+from datetime import date
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.exc import SQLAlchemyError
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+DATABASE_URL = f"sqlite:///{os.path.join(basedir, 'library.db')}"
+engine = create_engine(DATABASE_URL)
+
+class Base(DeclarativeBase):
+   pass
+
+class BaseService:
+
+   @staticmethod
+   def create(session, instance):
+      try:
+         session.add(instance)
+         session.commit()
+         session.refresh(instance)
+         return instance
+      except SQLAlchemyError:
+         session.rollback()
+         raise
+   
+   @staticmethod
+   def update(session):
+      try:
+         session.commit()
+      except SQLAlchemyError:
+         session.rollback()
+         raise
+   
+   @staticmethod
+   def delete(session, instance):
+      try:
+         session.delete(instance)
+         session.commit()
+      except SQLAlchemyError:
+         session.rollback()
+         raise
+
+   @staticmethod
+   def check_free_loan(session, book_id) -> bool:
+        loan = session.query(Loan).filter(
+            Loan.book_id == book_id,
+            Loan.return_date == None
+        ).first()
+        
+        return loan is None
+   
+class Book(Base):
+   __tablename__ = "book"
+   id = Column(Integer, primary_key=True)
+   title = Column(UnicodeText, unique=True, nullable=False)
+   author = Column(Unicode(64), unique=False, nullable=False)
+
+class Reader(Base):
+   __tablename__ = "reader"
+   id = Column(Integer, primary_key=True)
+   readers_name = Column(Unicode(64), unique=True, nullable=False)
+
+class Loan(Base):
+   __tablename__ = "loan"
+   id = Column(Integer, primary_key=True)
+   book_id = Column(Integer, ForeignKey("book.id"), nullable=False)
+   reader_id = Column(Integer, ForeignKey("reader.id"), nullable=False)
+   loan_date = Column(Date, default=date.today, nullable=False)
+   return_date = Column(Date, nullable=True)
+
+Base.metadata.create_all(engine)
